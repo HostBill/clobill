bf966d551df82619c7ce6fd6cd3c59915a07d573
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-04-17 10:10:39 +0200

    preparing for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clobill "0.1.0-SNAPSHOT"
+(defproject clobill "0.1.0"
   :description "clojure hostbill client"
   :url "https://github.com/exoscale/clobill"
   :license {:name "Eclipse Public License"

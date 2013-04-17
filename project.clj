f3d22f24750af8af44e8391cd047ce0b5df2d2b1
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-04-17 14:38:27 +0200

    allow http redirect responses

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clobill "0.1.3"
+(defproject clobill "0.1.4"
   :description "clojure hostbill client"
   :url "https://github.com/exoscale/clobill"
   :license {:name "Eclipse Public License"
